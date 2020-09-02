39092f16b33c214826aff44df01ed7a1c59d9a46
jpg333 <joshpgriffith@gmail.com>
2020-09-01 22:25:08 -0400

    Submission for grade and feedback

--- main.py
+++ main.py
@@ -4,68 +4,102 @@
 # imports
 import math
 import random
+import sys
 
 # introductory message
 print("This program will generate a random array based on parameters you choose."
-      "\nYou then will be able to perform various operations on the array")
+      "\nYou then will be able to perform various operations on the array.\n")
 
 
 # print list of array operations
 def opMenu(arr):
     # list of available operations. Record user choice
-    op = input("\nAvailable operations for array:"
-               "\n1. Sort array in ascending order"
-               "\n2. Sort array in descending order"
-               "\n3. Sum all numbers in array"
-               "\n4. Calculate product of all numbers in array"
-               "\n5. Calculate the average of all numbers in array"
-               "\n6. Square each individual number in array"
-               "\n7. Calculate square root of each individual number in array"
-               "\n8. Determine the amount of even numbers versus odd numbers"
-               "\n9. Identify the prime numbers in the array"
-               "\n10. Convert the numbers in the array from base 10 to a different base "
-               "\nEnter the number of the operation you want to perform on the array: ")
+    print("\nYour array: " + str(arr) +
+          "\nAvailable operations:"
+          "\n1. Sort array in ascending order"
+          "\n2. Sort array in descending order"
+          "\n3. Sum all numbers in array"
+          "\n4. Calculate product of all numbers in array"
+          "\n5. Calculate the average of all numbers in array"
+          "\n6. Square each individual number in array"
+          "\n7. Calculate square root of each individual number in array"
+          "\n8. Determine the amount of even numbers versus odd numbers"
+          "\n9. Identify the prime numbers in the array"
+          "\n10. Convert the numbers in the array from base 10 to a different base ")
+
+    # Try/Except block to ensure that input is valid
+    while True:
+        try:
+            op = int(input("\nEnter the number of the operation you want to perform on the array: "))
+            if op < 1 or op > 10:
+                raise ValueError
+        except ValueError:
+            print("Error: Please choose an operation listed above by entering a number 1-10:")
+        else:
+            break
 
     # calls choice handling method and passes on array and operation choice
-    opChoice(arr, int(op))
+    opChoice(arr, op)
     return
 
 
 # function to handle choice of operation
 def opChoice(arr, choice):
+    # initialize empty array to store new array that may be created from an operation
+    arrNw = []
+
     # option 1 is a simple ascending sort
     if choice == 1:
+        # sort array
         arr.sort()
-        print("Array numbers sorted in ascending order is:\n" + str(arr))
+        # print sorted array
+        print("\nOriginal array sorted in ascending order is:\n" + str(arr))
 
     # option 2 is a descending sort
     elif choice == 2:
+        # reverse sort array
         arr.sort(reverse=True)
-        print("Array numbers sorted in descending order is:\n" + str(arr))
+        # print reverse sorted array
+        print("\nOriginal array with numbers sorted in descending order is:\n" + str(arr))
 
     # option 3 sums all elements using method sumArray()
+    #   no new array needed
     elif choice == 3:
+        print("\nOriginal array: " + str(arr))
         print("Sum of all numbers in the array is: " + str(sumArray(arr)))
 
     # option 4 calculates product of all elements using method productArray()
+    #   no new array needed
     elif choice == 4:
+        print("\nOriginal array: " + str(arr))
         print("Product of all numbers in the array is: " + str(productArray(arr)))
 
     # option 5 calculates the mathematical average of all elements using method sumArray()
     #   and then dividing the sum by the array size
+    #       no new array needed
     elif choice == 5:
-        print("Average value of all the array numbers is: " + str(sumArray(arr) / len(arr)))
+        print("\nOriginal array: " + str(arr))
+        print("Average value of all the numbers in the array is: " + str(sumArray(arr) / len(arr)))
 
     # option 6 returns a new array determined by squaring all elements of the original array using method squareArray()
     elif choice == 6:
-        print("New array after all its numbers were squared:\n" + str(squareArray(arr)))
+        # set new array to the result of squareArray()
+        arrNw = squareArray(arr)
+        # print original as well as new array
+        print("Original array:\n" + str(arr))
+        print("\nNew array after all its numbers were squared:\n" + str(arrNw))
 
     # option 7 returns a new array determined by taking the square root of all elements of the original array
     #   using method sqrtArray()
     elif choice == 7:
-        print("New array after all its numbers were square rooted:\n" + str(sqrtArray(arr)))
+        # set new array to the result of sqrtArray()
+        arrNw = sqrtArray(arr)
+        # print original as well as new array
+        print("Original array:\n" + str(arr))
+        print("\nNew array after all its numbers were square rooted:\n" + str(arrNw))
 
     # option 8 counts the number of even and odd numbers in the array by using method evenOdd()
+    #   no new array needed
     elif choice == 8:
         # evenOdd() has 2 return values, where [0] is the even number and [1] is the odd
         even = evenOdd(arr)[0]
@@ -76,7 +110,11 @@ def opChoice(arr, choice):
     # option 9 returns a subset of the original array that contains only prime numbers using method primeCount().
     #   also displays the amount of primes via the length of the primes array
     elif choice == 9:
-        print(str(len(primeCount(arr))) + " prime numbers were found in the array:\n" + str(primeCount(arr)))
+        # set new array to the result of primeCount()
+        arrNw = primeCount(arr)
+        # print original as well as new array
+        print("Original array:\n" + str(arr) + "\n")
+        print(str(len(arrNw)) + " prime numbers were found in the array:\n" + str(arrNw))
 
     # option 10 returns a new array determined by converting all elements in the original array from base 10 to
     #   a different base (2-32) using method baseConvertArray()
@@ -86,19 +124,19 @@ def opChoice(arr, choice):
         while int(base) < 2 or int(base) > 32:
             base = input("Error: please enter a desired base between 2 and 32 (inclusive): ")
 
-        print("The array converted into base " + base + " is:\n" + str(baseConvertArray(arr, base)))
+        # set new array to the result of baseConvertArray()
+        arrNw = baseConvertArray(arr, base)
+        # print original as well as new array
+        print("\nOriginal array:\n" + str(arr))
+        print("\nThe array converted into base " + base + " is:\n" + str(arrNw))
 
-    # if no valid option is selected, output error message with another prompt for a valid option.
-    #   then recursive call to opChoice()
-    else:
-        print(choice)
-        retry = input("\nPlease choose an operation listed above by entering a number 1-10: ")
-        opChoice(arr, int(retry))
+    # after any operation is performed, take user to end() method, passing original array, new array, and number of
+    #   the operation that was just performed
+    end(arr, arrNw, choice)
 
 
 # driving method from which other functions are chained
 def start():
-
     # initial selection
     print("   Array Type Selection:\n"
           "1. Build your own array\n"
@@ -108,45 +146,81 @@ def start():
     # decision handling for user input or randomly generated array
     if answer == '1':
         array = byoArray()
+        print("Your array is: " + str(array))
     elif answer == '2':
         array = randArray()
     else:
         return 0
 
-    # ask if user is satisfied with the array
-    keep = input("Are you satisfied with your array?\n"
-                 "(Enter 'y' to proceed or 'n' to return to array creation): \n")
+    # check if user is satisfied with the array
+    print("\nAre you satisfied with your array?")
+    # Try/Except block to ensure that input is valid
+    while True:
+        try:
+            keep = input("(Enter 'y' to proceed or 'n' to return to array creation): ")
+            if keep not in ("y", "Y", "n", "N"):
+                raise ValueError
+        except ValueError:
+            print("Error: please enter 'y' or 'n'")
+        else:
+            break
 
     # call keepArray() to evaluate if array will be rerolled or not
     if keepArray(keep):
+        # if yes, proceed to operation choice
         opMenu(array)
     else:
         # unhappy with array --> restart array selection
         start()
 
-    return
-
 
 # method for user to build their own array
+# unsure of how to structure try/except to ensure that list is input correctly
 def byoArray():
-    return
+    string = input("Enter the integers for your array one at a time, separated by spaces: ")
+    arrInt = []
+    arrString = string.split(" ")
+
+    for s in arrString:
+        arrInt.append(int(s))
+
+    return arrInt
 
 
 # initialize array with size of array as well as min and max values for the random generator range
 def randArray():
     arr = []
+
     # gather array constraints from user input
-    size = int(input("Enter array size: "))
-    low = int(input("Enter lowest possible value for the array: "))
-    high = int(input("Enter highest possible value for the array: "))
+    # Try/Except block to ensure that input is valid
+    while True:
+        try:
+            size = int(input("Enter array size: "))
+            if size < 1:
+                raise ValueError
+        except ValueError:
+            print("Error: array size must be greater than 0")
+        else:
+            break
+
+    while True:
+        try:
+            low = int(input("Enter the minimum value for the array: "))
+            high = int(input("Enter the maximum value for the array: "))
+            if low > high:
+                raise ValueError
+        except ValueError:
+            print("Error: the maximum value cannot be less than the minimum value")
+        else:
+            break
 
     # build array by appending n number of random elements
     for i in range(size):
-        arr.append(random.randint(low, high+1))
+        arr.append(random.randint(low, high))
     # print array
-    print("Array size: " + str(size) + "   Minimum possible value: " + str(low) +
+    print("\nArray size: " + str(size) + "   Minimum possible value: " + str(low) +
           "   Maximum possible value: " + str(high))
-    print("\nRandom Array: " + str(arr))
+    print("Random Array: " + str(arr))
     return arr
 
 
@@ -158,6 +232,73 @@ def keepArray(st):
     # false if user wants new array
     elif st == 'n' or st == 'N':
         return False
+
+
+# method with options to restart, quit, or proceed with another operation
+def end(arrayOg, arrayNw, prevOp):
+    # operations 1, 2, 6, 7, 9, and 10 output a new array, so users will have one additional option after performing
+    #   one of these operations. They may continue to do operations with the new array
+    if prevOp in (6, 7, 9, 10):
+        print("\nWould you like to:\n"
+              "1. Perform another operation with your original array?\n"
+              "2. Perform an operation with your new array?\n"
+              "3. Start over with a different array?\n"
+              "4. Exit application?")
+
+        # Try/Except block to ensure that input is valid
+        while True:
+            try:
+                endOp = input("(Enter 1, 2, 3, or 4): ")
+                if endOp not in ("1", "2", "3", "4"):
+                    raise ValueError
+            except ValueError:
+                print("Error: available choices are 1, 2, 3, and 4")
+            else:
+                break
+
+        # 4 different user options require 4 conditions and one validity check
+        if endOp == "1":
+            # pass original array to operation selection
+            opMenu(arrayOg)
+        elif endOp == "2":
+            # pass new array to operation selection
+            opMenu(arrayNw)
+            # restart array selection
+        elif endOp == "3":
+            start()
+        elif endOp == "4":
+            # terminate program
+            print("Bye!")
+            sys.exit()
+
+    # if the operation did not result in a newly created array, only three options are available
+    else:
+        print("\nWould you like to:\n"
+              "1. Perform another operation with your original array?\n"
+              "2. Start over with a different array?\n"
+              "3. Exit application?")
+
+        # Try/Except block to ensure that input is valid
+        while True:
+            try:
+                endOp = input("(Enter 1, 2, or 3): ")
+                if endOp not in ("1", "2", "3", "4"):
+                    raise ValueError
+            except ValueError:
+                print("Error: available choices are 1, 2, and 3")
+            else:
+                break
+
+        if endOp == "1":
+            # pass original array to operation selection
+            opMenu(arrayOg)
+        elif endOp == "2":
+            # restart array selection
+            start()
+        elif endOp == "3":
+            # terminate program
+            print("Bye!")
+            sys.exit()
 
 
 # calculates sum of array
@@ -300,4 +441,5 @@ def baseConvertArray(arr, b):
     return baseArr
 
 
+# call start function, which chains calls to other functions
 start()
